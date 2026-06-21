fba5a8d3105040ad5d5a5db0dd37ca02bccfe583
michaelsfx-qa <plusultrasfx1@gmail.com>
2026-06-20 20:48:29 -0400

    usar endpoint tasas del api en vez de dollar local

--- src/services/tasas.js
+++ src/services/tasas.js
@@ -1,18 +1,12 @@
 import axios from 'axios';
 
-export const obtenerTasasBcv = async () => {
-  const response = await axios.get('https://rates.dolarvzla.com/bcv/current.json');
+export const obtenerTasas = async () => {
+  const response = await axios.get(`${process.env.REACT_APP_API_URL}/tasas`);
   return {
-    usd: response.data.current.usd,
-    eur: response.data.current.eur,
+    bcv: {
+      usd: response.data.tasaBcvUsd,
+      eur: response.data.tasaBcvEur,
+    },
+    usdt: response.data.tasaUsdt,
   };
-};
-
-export const obtenerTasaUsdt = async () => {
-  const response = await axios.get('https://api.dolarvzla.com/public/usdt/exchange-rate', {
-    headers: {
-      'x-dolarvzla-key': 'c518977b485a9543e2edcd01230592bf2fbb81685eefd3e1e5b81aed7f970f81'
-    }
-  });
-  return response.data.current.sell;
 };
